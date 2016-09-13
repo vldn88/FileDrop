2b565840956593f5961a5c1923606b047f36adf5
Denis Valanovic <denis.valanovic@gmail.com>
2016-09-13 14:51:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+    <body>
+    <?php
+
+    $myName = "Lester Todorovic";
+
+    echo "Hello, my name is " , $myName;
+    
+	?>
+    </body>
+</html>
